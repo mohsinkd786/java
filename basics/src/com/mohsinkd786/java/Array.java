80bd0859a1d476b15cd2754fcd4319967f033ec5
Mohsin Khursheed <mohsinkd786@gmail.com>
2018-09-10 12:16:02 +0530

    Java Basics

--- basics/src/com/mohsinkd786/java/Array.java
+++ basics/src/com/mohsinkd786/java/Array.java
@@ -1,5 +1,7 @@
 package com.mohsinkd786.java;
 
+import java.util.Arrays;
+
 public class Array {
 
 	public static void main(String[] args) {
@@ -21,9 +23,18 @@ public class Array {
 		// Way 2
 		// Left : name & definition
 		// Right : assign values
-		String[] strs = { "Hello", "How", "r u" };
-		// will print Hello
+		String[] strs = { "Hoy", "Hey", "r u" };
+		// will print Hey
+		System.out.println(strs[1]);
+		// Sorting an Array
+		Arrays.sort(strs);
+		// after sorting the array
+		// values should be re ordered based on the alphabetical order
 		System.out.println(strs[1]);
 
+		double[] values = { 1.0, 1.1, 1.2 };
+		// creating new array from old array
+		double[] new_values = values;
+		System.out.println(new_values[2]);
 	}
 }
